8963df47070f1e8b18a5945489642e3d8cc1d1ea
Edwin <tazedwin@gmail.com>
2022-03-10 21:08:48 -0500

    refactorize code

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 
-type ColumnsItem = {
+export type ColumnsItem = {
     id: string,
     name: string,
     isSlot?: boolean,
-    callback: () => void
+    callback?: () => void
 }
 
 interface Props {
@@ -18,17 +18,25 @@ const props = defineProps<Props>();
 
 <template>
     <div class="table-container">
-        <table>
-            <thead>
-                <tr class="">
-                    <th v-for="column in props.columns" :key="column.id">{{ column.name }}</th>
+        <table class="min-w-full divide-y divide-gray-300">
+            <thead class="bg-gray-100">
+                <tr>
+                    <th
+                        class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-500 uppercase tracking-wider text-sm"
+                        v-for="column in props.columns" :key="column.id"
+                    >
+                        <span class="text-sm text-gray-500">
+                            {{ column.name }}
+                        </span>
+                    </th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="bg-white divide-y divide-gray-100 transition ease-in-out delay-10 hover:bg-gray-200">
                 <tr  v-for="item in props.data" :key="item.id">
                     <td
                         v-for="column in props.columns"
                         :key="column.id"
+                        class="px-6 py-4 whitespace-nowrap"
                         :class="{ 'isOptions': column.id === 'options' }"
                     >
                         <template
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,22 @@
 <template>
-  <div class="flex flex-col">
+<div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-1">
       <div class="py-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="shadow overflow-hidden border-b border-gray-500 sm:rounded-lg ">
-          <tr class="text-2xl">Uniswap</tr>
-          <table class="min-w-full divide-y divide-gray-300">
-            <thead class="bg-gray-100">
-              <tr>
-                <th scope="col" class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">Token</th>
-                <th scope="col" class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">Precio</th>
-                <th scope="col" class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">Intercambiar</th>
-              </tr>
-            </thead>
-            <tbody class="bg-white divide-y divide-gray-100 transition ease-in-out delay-10 hover:bg-gray-200 ">
-              <tr v-for="cripto in coinbase" :key="cripto.base">
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-500">{{ cripto.base }}</div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {{ cripto.price }}
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <button @click="falloIntercambio" class="transition ease-in-out delay-600 hover:-translate-y-1 hover:scale-200 duration-300">
+          <h2 class="px-2 py-3 text-2xl text-left">Uniswap</h2>
+          <Table
+            :columns="columns"
+            :data="uniswapPrices"
+          >
+            <template v-slot:price="{ item }">
+              {{ item.price }} {{ item.currency }}
+            </template>
+            <template v-slot:options>
+              <button @click="mockExchangeSuccess" class="transition ease-in-out delay-600 hover:-translate-y-1 hover:scale-200 duration-300">
                   Cambiar
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+              </button>
+            </template>
+          </Table>
         </div>
       </div>
     </div>
@@ -36,80 +25,88 @@
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-1">
       <div class="py-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="shadow overflow-hidden border-b border-gray-500 sm:rounded-lg ">
-          <tr class="text-2xl">Binace Smart Chain</tr>
-          {{ coinbase }}
-          <table class="min-w-full divide-y divide-gray-300">
-            <thead class="bg-gray-100">
-              <tr>
-                <th scope="col" class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">Token</th>
-                <th scope="col" class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">Precio</th>
-                <th scope="col" class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-500 uppercase tracking-wider text-sm">Intercambiar</th>
-              </tr>
-            </thead>
-            <tbody class="bg-white divide-y divide-gray-100 transition ease-in-out delay-10 hover:bg-gray-200 ">
-              <tr v-for="cripto in uniswap" :key="cripto.base">
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-500">{{ cripto.base }}</div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {{ cripto.price }}
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <button @click="falloIntercambio" class="transition ease-in-out delay-600 hover:-translate-y-1 hover:scale-200 duration-300">
-                    Cambiar
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <h2 class="px-2 py-3 text-2xl text-left">Coinbase</h2>
+          <Table
+            :columns="columns"
+            :data="coinbasePrices"
+          >
+            <template v-slot:price="{ item }">
+              {{ item.price }} {{ item.currency }}
+            </template>
+            <template v-slot:options>
+              <button @click="mockExchangeFailed" class="transition ease-in-out delay-600 hover:-translate-y-1 hover:scale-200 duration-300">
+                  Cambiar
+              </button>
+            </template>
+          </Table>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
+<script setup lang="ts">
 /* eslint-disable */
-import { defineComponent, onMounted } from 'vue';
+import Table from '../components/Table.vue';
+import { onSnapshot, query } from "firebase/firestore";
+import { db, collection, getDocs } from '../utils/firebase';
+import { onMounted, ref, computed } from 'vue';
 import Swal from 'sweetalert2';
-import { db, collection, getDocs } from '../utils/firebase.js';
-import { doc, onSnapshot } from "firebase/firestore";
 
-let coinbase = [];
-let uniswap = [];
-export default defineComponent({
-  setup() {
-    async function getCoinbase(DB) {
-    const coinbaseCol = collection(DB, 'coinbase');
-    const coinSnapshot = await getDocs(coinbaseCol);
-    const coinList = coinSnapshot.docs.map((doc) => doc.data());
-    console.log(coinList);
-    return coinList;
-    }
-    async function getUniswap(DB) {
-    const uniswapCol = collection(DB, 'uniswap');
-    const unisSnapshot = await getDocs(uniswapCol);
-    const unisList = unisSnapshot.docs.map((doc) => doc.data());
-    console.log(unisList);
-    return unisList;
-    }
-    onMounted(async () => {
-    coinbase = await getCoinbase(db);
-    uniswap = await getUniswap(db);
-    console.log(coinbase);
-    });
-    const falloIntercambio = () => {
-      Swal.fire({
-        title: 'OPPS',
-        text: 'Intercambio fallido',
-        icon: 'warning',
-      });
-    };
-    return {
-      coinbase,
-      uniswap,
-      falloIntercambio,
-    };
+const coinbasePrices = ref([])
+const uniswapPrices = ref([])
+
+const columns = computed(() => [
+  {
+    id: "pairName",
+    name: "Par Asset"
   },
+  {
+    id: "price",
+    name: "Precio",
+    isSlot: true
+  },
+  {
+    id: "options",
+    name: "Intercambiar",
+    isSlot: true
+  },
+])
+
+
+
+const fetchDataFromFirebase = (collectionName: string, listOutput: any) => {
+  const q = query(collection(db, collectionName));
+    onSnapshot(q, (querySnapshot) => {
+      const list = [] as any
+      querySnapshot.forEach((doc) => {
+          list.push(doc.data())
+      });
+      console.log(list)
+  
+      listOutput.value = list
+  
+    });
+}
+
+onMounted(() => {
+  fetchDataFromFirebase('coinbase', coinbasePrices)
+  fetchDataFromFirebase('uniswap', uniswapPrices)
 });
+
+const mockExchangeSuccess = () => {
+  Swal.fire({
+    title: 'Exitoso',
+    text: 'Intercambio Exitoso',
+    icon: 'success',
+  });
+};
+
+const mockExchangeFailed = () => {
+  Swal.fire({
+    title: 'OPPS',
+    text: 'Intercambio fallido',
+    icon: 'warning',
+  });
+};
+
 </script>
